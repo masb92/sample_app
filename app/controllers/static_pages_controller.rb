78a2e463d0b07ca532c9d3ffd23d0172df78125e
Martin <masb92@gmail.com>
2013-01-25 19:18:18 +0100

    Add a full_title helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,18 @@
 class StaticPagesController < ApplicationController
+  
   def home
+    @users = User.all
   end
 
   def help
+    user = User.new
+    if user.save!
+      flash[:notice] = "Success"
+    else
+      flash[:notice] = "Ble ikke lagret"
+    end
+  end
+  
+  def about
   end
 end
