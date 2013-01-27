a0b062e82116b334ca191126066660ad608b13ba
Martin <masb92@gmail.com>
2013-01-27 15:15:39 +0100

    Finished layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,14 @@
 class StaticPagesController < ApplicationController
   
   def home
-    @users = User.all
   end
 
   def help
-    user = User.new
-    if user.save!
-      flash[:notice] = "Success"
-    else
-      flash[:notice] = "Ble ikke lagret"
-    end
   end
   
   def about
+  end
+  
+  def contact
   end
 end
